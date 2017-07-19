83fef3b3a31e0a19d881e1134a43bd7bee305a54
jack2gs <sailor.gao@163.com>
2017-07-20 07:46:28 +0800

    init

---
+++ Fnz.Model.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Fnz.Model.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Fnz.Mvc.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Fnz.Mvc.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
